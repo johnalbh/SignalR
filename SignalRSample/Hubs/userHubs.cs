ec778401c8941d0c1f778566bc195307a25275ec
John Alberto López Hernández <johnlopezhernandez@outlook.com>
2022-09-19 16:16:59 +0200

    Creación Inicial SignalR

---
+++ SignalRSample/Hubs/userHubs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+    public class userHubs: Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            // Send update to all client that total views have been updated
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
+    }
+}
