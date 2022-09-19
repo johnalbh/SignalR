6981bb40299315d7812c7f08cdac2408e8b745ba
John Alberto López Hernández <johnlopezhernandez@outlook.com>
2022-09-19 16:44:54 +0200

    Connect to SignalR Hub from Client JS

---
+++ SignalRSample/wwwroot/js/userCount.js
@@ -0,0 +1,25 @@
+﻿//Create Connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+// Connect to methods that hub invokes aka receive notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerHTML = value.toString();
+});
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+// start connection
+function fulfilled() {
+    // do something on start
+    console.log("Connection Ok");
+}
+function rejected() {
+    //rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
+
